2ef41434893ed24e390d7faa94f0dad0a6567a10
Coteh <james_c_34@live.com>
2018-03-24 14:23:56 -0400

    Add add wallet

--- web/app/containers/Wallet/actions.js
+++ web/app/containers/Wallet/actions.js
@@ -1,7 +1,13 @@
-import { LOAD_WALLETS } from './constants';
+import { LOAD_WALLETS, ADD_WALLET } from './constants';
 
 export function loadWallets() {
   return {
     type: LOAD_WALLETS,
+  };
+}
+
+export function addWallet() {
+  return {
+    type: ADD_WALLET
   };
 }
